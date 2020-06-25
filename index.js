c506a58ecd2ea0f252d6f96d8d6ffad63e7cc417
Beth Ruff <beth.ruff2@gmail.com>
2020-06-24 21:11:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// let writeCards = [ 'Lisa', 'Kaitlin', 'Jan' ]
+// let names = ['Lisa', 'Kaitlin', 'Jan'];
+function writeCards(names) {
+    const newArray = []
+    for (let i = 0; i <= names.length - 1; i++) {
+      newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return newArray;
+  }
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
